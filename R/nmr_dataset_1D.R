bc0a3cd32ec0b8c66f494d76bcccbfed95094915
Sergio Oller <soller@ibecbarcelona.eu>
2018-10-25 17:02:12 +0200

    nmr_dataset_1D$metadata is a list of data frames

--- R/nmr_dataset_1D.R
+++ R/nmr_dataset_1D.R
@@ -15,17 +15,7 @@ validate_nmr_dataset_1D <- function(nmr_dataset_1D) {
               msg = "nmr_dataset_1D must have a ppm axis")
   assert_that("data_1r" %in% names(nmr_dataset_1D),
               msg = "nmr_dataset_1D must have a data_1r matrix")
-  
-  assert_that("metadata" %in% names(nmr_dataset_1D), msg = "Missing acquisition and parameter metadata")
-  assert_that("metadata_ext" %in% names(nmr_dataset_1D), msg = "Missing external metadata")
-  metadata <- nmr_dataset_1D[["metadata"]]
-  metadata_ext <- nmr_dataset_1D[["metadata_ext"]]
-  assert_that(is.data.frame(metadata), msg = "metadata should be a data frame")
-  assert_that(is.data.frame(metadata_ext), msg = "metadata_ext should be a data frame")
-  assert_that("NMRExperiment" %in% colnames(metadata), msg = "metadata should have an NMRExperiment column")
-  assert_that("NMRExperiment" %in% colnames(metadata_ext), msg = "metadata_ext should have an NMRExperiment column")
-  assert_that(nrow(metadata) == nrow(metadata_ext), msg = "metadata and metadata_ext should have the same number of rows")
-  
+
   ppm_axis <- nmr_dataset_1D[["axis"]]
   data_1r <- nmr_dataset_1D[["data_1r"]]
   assert_that(is.vector(ppm_axis) && is.numeric(ppm_axis),
@@ -39,26 +29,40 @@ validate_nmr_dataset_1D <- function(nmr_dataset_1D) {
   
   assert_that(num_samples == nmr_dataset_1D[["num_samples"]],
               msg = "The num_samples value does not match nrow(data_1r)")
-  assert_that(num_samples == nrow(metadata),
-              msg = "data_1r and acquisition metadata differ in number of samples")
-  assert_that(nrow(metadata_ext) == nrow(metadata),
-              msg = "Acquisition metadata and external metadata differ in number of rows")
-  nmr_dataset_1D
+  
+  
+  assert_that("metadata" %in% names(nmr_dataset_1D), msg = "Missing acquisition and parameter metadata")
+  metadata <- nmr_dataset_1D[["metadata"]]
+  assert_that(is.vector(metadata) & is.list(metadata), msg = "metadata should be a list")
+  if (length(metadata) > 0) {
+    assert_that(all(purrr::map_lgl(metadata, is.data.frame)), msg = "all metadata elements should be data frames")
+    for (metad_idx in seq_along(metadata)) {
+      metad_name <- names(metadata)[metad_idx]
+      metad <- metadata[[metad_idx]]
+      assert_that(nrow(metad) == num_samples,
+                  msg = glue::glue("The number of rows of {metad_name} does not match the number of samples"))
+      assert_that("NMRExperiment" %in% colnames(metad),
+                  msg = glue::glue_data(
+                    list(metad_name = metad_name),
+                    "metadata '{metad_name}' does not include the NMRExperiment column"))
+      assert_that(all(metad[["NMRExperiment"]] == metadata[[1]][["NMRExperiment"]]),
+                  msg = glue::glue("The NMRExperiment column in {metad_name} is not equal the same column in {names(metadata)[1]}"))
+    }
+  }
+    nmr_dataset_1D
 }
 
 #' Creates a new 1D nmr_dataset object from scratch
 #' 
 #' @param ppm_axis A numeric vector with the ppm values for the columns of data_1r
 #' @param data_1r A numeric matrix with one NMR spectrum on each row
-#' @param metadata A data frame with at least the `NMRExperiment` column
-#' @param metadata_ext A data frame with at least the `NMRExperiment` column
+#' @param metadata A list of data frames with at least the `NMRExperiment` column
 #' 
 #' @importFrom assertthat assert_that
 #' @importFrom glue glue
 #' @export
-new_nmr_dataset_1D <- function(ppm_axis, data_1r, metadata, metadata_ext) {
+new_nmr_dataset_1D <- function(ppm_axis, data_1r, metadata) {
   samples <- list()
-  samples[["metadata_ext"]] <- metadata_ext
   samples[["metadata"]] <- metadata
   samples[["data_1r"]] <- data_1r
   samples[["axis"]] <- ppm_axis
