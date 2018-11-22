e9755d41cdedaa268f8daf426e26745c040d34d3
Sergio Oller <soller@ibecbarcelona.eu>
2018-11-22 08:41:47 +0100

    nmr_data<- method

--- R/nmr_data.R
+++ R/nmr_data.R
@@ -1,5 +1,5 @@
 
-#' Return the full spectra matrix
+#' Set/Return the full spectra matrix
 #'
 #' @param nmr_dataset The NMR object to get the raw data from
 #' @param ... Unused and left for future compatibility
@@ -18,4 +18,25 @@ nmr_data.nmr_dataset_1D <- function(nmr_dataset, ...) {
   rownames(data_1r) <- nmr_meta_get_column(nmr_dataset, "NMRExperiment")
   colnames(data_1r) <- nmr_dataset$axis
   data_1r
+}
+
+
+#' @rdname nmr_data
+#' @param value A matrix
+#'
+#' @return The given nmr_dataset
+#' @export
+#'
+"nmr_data<-" <- function(nmr_dataset, value) {
+  UseMethod("nmr_data<-")
+}
+
+#' @rdname nmr_data
+#' @export
+"nmr_data<-.nmr_dataset_1D" <- function(nmr_dataset, value) {
+  value <- as.matrix(value)
+  stopifnot(nrow(value) == nmr_dataset$num_samples)
+  rownames(value) <- colnames(value) <- NULL
+  nmr_dataset$data_1r <- value
+  nmr_dataset
 }
