e622ac88a4958994a4cc33446288c3f7c25319f5
Sergio Oller <soller@ibecbarcelona.eu>
2018-12-03 10:21:46 +0100

    nmr_meta_add_tidy_excel

--- R/nmr_meta.R
+++ R/nmr_meta.R
@@ -1,6 +1,5 @@
 #' Get metadata
 #' 
-#' @family metadata functions
 #' @param samples a [nmr_dataset] object
 #' @param columns Columns to get. By default gets all the columns.
 #' @param groups Groups to get. Groups are predefined of columns. Typically 
@@ -9,7 +8,7 @@
 #' Both `groups` and `columns` can't be given simultaneously.
 #' 
 #' @return a data frame with the injection metadata
-#' @family metadata management functions
+#' @family metadata functions
 #' @family nmr_dataset functions
 #' @family nmr_dataset_1D functions
 #' @family nmr_dataset_peak_table functions
@@ -65,11 +64,10 @@ nmr_meta_get <- function(samples, columns = NULL, groups = NULL) {
 
 #' Get a single metadata column
 #' 
-#' @family metadata functions
-#' @param samples a [nmr_dataset] object
+#' @param samples a [nmr_dataset_family] object
 #' @param column A column to get
 #' @return A vector with the column
-#' @family metadata management functions
+#' @family metadata functions
 #' @family nmr_dataset functions
 #' @family nmr_dataset_1D functions
 #' @family nmr_dataset_peak_table functions
@@ -83,7 +81,6 @@ nmr_meta_get_column <- function(samples, column = "NMRExperiment") {
 #' This is useful to add metadata to datasets that can be later used for
 #' plotting spectra or further analysis (PCA...).
 #' 
-#' @family metadata functions
 #' @param nmr_data an [nmr_dataset] object
 #' @param metadata A data frame with metadata to add
 #' @param by A column name of both the `nmr_dataset$metadata$external` and the metadata
@@ -118,11 +115,10 @@ nmr_meta_get_column <- function(samples, column = "NMRExperiment") {
 #' # The final loaded metadata:
 #' print(nmr_meta_get(nmr_dataset, groups = "external"))
 #' 
-#' @family metadata management functions
+#' @family metadata functions
 #' @family nmr_dataset functions
 #' @family nmr_dataset_1D functions
 #' @family nmr_dataset_peak_table functions
-#' @family import/export functions
 nmr_meta_add <- function(nmr_data, metadata, by = "NMRExperiment") {
   nmr_meta <- nmr_meta_get(nmr_data, groups = "external")
   by_left <- ifelse(is.null(names(by)), by, names(by))
@@ -144,16 +140,71 @@ nmr_meta_add <- function(nmr_data, metadata, by = "NMRExperiment") {
   nmr_data
 }
 
+#' @rdname nmr_meta_add
+#'
+#' @param excel_file Path to a tidy Excel file name. The Excel can consist
+#' of multiple sheets, that are added sequentially. The first column of the first
+#' sheet MUST be named as one of the metadata already present in the dataset,
+#' typically will be "NMRExperiment". The rest of the columns of the first sheet
+#' can be named at will. Similary, the first column of the second sheet must be
+#' named as one of the metadata already present in the dataset, typically
+#' "NMRExperiment" or any of the columns of the first sheet. The rest of the columns
+#' of the second sheet can be named at will. See the package vignette for an
+#' example.
+#' 
+#' @export
+#' @examples 
+#' # Read a tidy excel file:
+#' 
+#' dataset <- system.file("dataset-demo", package = "NIHSnmr")
+#' nmr_dataset <- nmr_read_samples_dir(dataset)
+#' 
+#' # At first we just have the NMRExperiment column
+#' print(nmr_meta_get(nmr_dataset, groups = "external"))
+#' # Get a table with NMRExperiment -> SubjectID
+#' dummy_metadata <- system.file("dataset-demo", "dummy_metadata.xlsx", package = "NIHSnmr")
+#' 
+#' nmr_dataset <-nmr_meta_add_tidy_excel(nmr_dataset, dummy_metadata)
+#' # Updated Metadata:
+#' print(nmr_meta_get(nmr_dataset, groups = "external"))
+nmr_meta_add_tidy_excel <- function(nmr_data, excel_file) {
+  excel_sheets <- readxl::excel_sheets(excel_file)
+  excel_dfs <- purrr::map(excel_sheets, ~readxl::read_excel(path = excel_file, sheet = .))
+  
+  for (excel_df in excel_dfs) {
+    # If the sheet is empty, skip
+    if (nrow(excel_df) == 0 || ncol(excel_df) == 0) {
+      # Empty sheet
+      next
+    }
+    # Check that the key column is already in the metadata:
+    key_column <- colnames(excel_df)[1]
+    avail_columns <- colnames(nmr_meta_get(nmr_data, groups = "external"))
+    if (!key_column %in% avail_columns) {
+      stop(glue::glue("Key Column {key_column} is not present in the metadata.",
+                      "Can't add metadata.\n Available columns:",
+                      glue::glue_collapse(avail_columns, sep = ", ", last = " and ")))
+    }
+    # The NMRExperiment column should of type character, although Excel may use numeric
+    # because usually all the identifiers are numeric. We convert it manually:
+    if ("NMRExperiment" %in% colnames(excel_df)) {
+      excel_df$NMRExperiment <- as.character(excel_df$NMRExperiment)
+    }
+    # Add the metadata
+    nmr_data <- nmr_meta_add(nmr_data, metadata = excel_df, by = key_column)
+  }
+  nmr_data
+}
+
 #' Export Metadata to an Excel file
 #'
-#' @family metadata functions
 #' @param nmr_dataset An nmr_dataset object
 #' @param xlsx_file "The .xlsx excel file"
 #' @param groups A character vector. Use `"external"` for the external metadata or
 #'  the default for a more generic solution
 #' @return The Excel file name
 #' @export
-#' @family metadata management functions
+#' @family metadata functions
 #' @family nmr_dataset functions
 #' @family nmr_dataset_1D functions
 #' @family nmr_dataset_peak_table functions
