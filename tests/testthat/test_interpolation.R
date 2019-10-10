83f87c1ef9bfdcfcf03c837784da418a47bf2f81
Francisco Madrid-Gambin <35764822+Francisco-madrid-gambin@users.noreply.github.com>
2019-10-10 18:08:17 +0200

    test for AlpsNMR filters

--- tests/testthat/test_interpolation.R
+++ tests/testthat/test_interpolation.R
@@ -1,16 +1,9 @@
 context("test-interpolation")
 
 test_that("nmr_ppm_resolution works", {
-  dir_to_demo_dataset <- system.file("dataset-demo", package = "AlpsNMR")
-  dataset <- nmr_read_samples_dir(dir_to_demo_dataset)
-  ppm_res <- nmr_ppm_resolution(dataset)[[1]]
-  expect_true(is.numeric(ppm_res))
-})
-
-
-test_that("nmr_ppm_resolution works", {
-  dir_to_demo_dataset <- system.file("dataset-demo", package = "AlpsNMR")
-  dataset <- nmr_read_samples_dir(dir_to_demo_dataset)
+  dataset <- new_nmr_dataset_1D(ppm_axis = c(1,2,3),
+                                data_1r = matrix(c(1,2,1), nrow = 1),
+                                metadata = list(external = data.frame(NMRExperiment = "10")))
   ppm_res <- nmr_ppm_resolution(dataset)[[1]]
   expect_true(is.numeric(ppm_res))
 })
