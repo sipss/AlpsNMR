87e9e23186675b5f47b081819a078d71106ca9b1
Francisco Madrid-Gambin <35764822+Francisco-madrid-gambin@users.noreply.github.com>
2019-10-16 16:57:58 +0200

    Tests for nmr_baseline_threshold and  pipeline

--- tests/testthat/test-baseline_removal.R
+++ tests/testthat/test-baseline_removal.R
@@ -7,3 +7,13 @@ test_that("nmr_baseline_removal works", {
   dataset <- nmr_baseline_removal(dataset, lambda = 4, p = 0.02)
   expect_true(is.matrix(dataset[["data_1r"]]))
 })
+
+test_that("nmr_baseline_threshold works", {
+  dataset <- new_nmr_dataset_1D(ppm_axis = c(9.5, 9.7, 10.0),
+                                data_1r = matrix(sample(0:2, 6, replace = TRUE), nrow = 2),
+                                metadata = list(external = data.frame(NMRExperiment = c("10","20"))))
+  
+  n <- nmr_baseline_threshold (dataset)
+  expect_true(is.numeric(n))
+})
+  
