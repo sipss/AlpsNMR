fe5a08fb266c75ff6c4fc48e65e09516bebc0530
Francisco Madrid-Gambin <35764822+Francisco-madrid-gambin@users.noreply.github.com>
2019-10-16 17:13:50 +0200

    tests for nmr_data and nmr_dataset

--- tests/testthat/test-nmr_data.R
+++ tests/testthat/test-nmr_data.R
@@ -1,10 +1,10 @@
-context("test-new_nmr_dataset_1d")
+context("test-nmr_data")
 
 test_that("new_nmr_dataset_1D", {
   ds <- new_nmr_dataset_1D(ppm_axis = c(1,2,3),
                            data_1r = matrix(c(1,2,1), nrow = 1),
                            metadata = list(external = data.frame(NMRExperiment = "10")))
-  
-  expect_equal(ds[["num_samples"]], 1)
+  dataset <- nmr_data(ds)
+  expect_true(is.matrix(dataset))
 })
 
--- tests/testthat/test-outliers.R
+++ tests/testthat/test-outliers.R
@@ -2,7 +2,7 @@ context("test-outliers")
 
 test_that("nmr_pca_outliers_robust works", {
   dataset <- new_nmr_dataset_1D(ppm_axis = c(0:10),
-                                data_1r = matrix(sample(0:43,replace = TRUE), nrow = 4),
+                                data_1r = matrix(sample(0:43,replace = FALSE), nrow = 4),
                                 metadata = list(external = data.frame(NMRExperiment = c("10", "20", "30","40"))))
   # dataset[["metadata"]][["external"]][["NMRExperiment"]] = as.character(dataset[["metadata"]][["external"]][["NMRExperiment"]])
   pca_outliers <- nmr_pca_outliers_robust(dataset)
