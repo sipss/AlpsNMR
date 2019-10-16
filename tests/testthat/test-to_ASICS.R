a999507c4bd20ece68a1f19b3647d55abeb4b046
Francisco Madrid-Gambin <35764822+Francisco-madrid-gambin@users.noreply.github.com>
2019-10-16 18:42:38 +0200

    Create test-to_ASICS.R

---
+++ tests/testthat/test-to_ASICS.R
@@ -0,0 +1,9 @@
+context("test-to_ASICS")
+
+test_that("to_ASICS", {
+dataset <- new_nmr_dataset_1D(ppm_axis = c(0:10),
+                              data_1r = matrix(sample(0:43,replace = FALSE), nrow = 4),
+                              metadata = list(external = data.frame(NMRExperiment = c("10", "20", "30","40"))))
+asics = to_ASICS(dataset)
+expect_true(is.integer((asics@spectra@Dim)))
+})
