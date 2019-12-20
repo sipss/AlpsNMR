dc6bea5754e4764a61232733d7c3698edd2ae050
Francisco Madrid-Gambin <35764822+Francisco-madrid-gambin@users.noreply.github.com>
2019-12-20 12:37:48 +0100

    Fixing Travis III

--- R/download_MTBLS242_demo.R
+++ R/download_MTBLS242_demo.R
@@ -4,12 +4,6 @@
 #'
 #' @return A folder with demo samples
 #' @export
-#' @examples
-#' \dontrun{
-#' download_demo(to = "C:/Users/")
-#' }
-#' 
-
 download_demo <- function(to = ".") {
   if (!dir.exists(to)) {
     dir.create(to, recursive = TRUE)
@@ -17,10 +11,6 @@ download_demo <- function(to = ".") {
   zip_fn <- file.path(to, "MTBLS242.zip")
   dropbox_link <- "https://dl.dropboxusercontent.com/s/0snivrsd7m82yey/MTBLS242.zip?dl=0"
   utils::download.file(dropbox_link, method = "auto", destfile = zip_fn, mode = "wb")
-  
-  # output <- file.path(to, "MTBLS242")
-  # fs::dir_create(output)
-  
   zip::unzip(zipfile = zip_fn, exdir = to)
   invisible(NULL)
 }
