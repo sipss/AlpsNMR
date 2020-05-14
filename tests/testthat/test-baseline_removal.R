5f368d5833a4f73ef9515c1fb51706355ab979b8
Hector Gracia <hgracia@ibecbarcelona.eu>
2020-05-14 17:49:44 +0200

    Corregido test baseline removal

--- tests/testthat/test-baseline_removal.R
+++ tests/testthat/test-baseline_removal.R
@@ -1,8 +1,8 @@
 context("test-baseline_removal")
 
 test_that("nmr_baseline_removal works", {
-  dataset <- new_nmr_dataset_1D(ppm_axis = c(1,2,3),
-                                data_1r = matrix(c(1,2,1), nrow = 1),
+  dataset <- new_nmr_dataset_1D(ppm_axis = c(1,2,3,4),
+                                data_1r = matrix(c(1,2,2,1), nrow = 1),
                                 metadata = list(external = data.frame(NMRExperiment = "10")))
   dataset <- nmr_baseline_removal(dataset, lambda = 4, p = 0.02)
   expect_true(is.matrix(dataset[["data_1r"]]))
@@ -16,4 +16,3 @@ test_that("nmr_baseline_threshold works", {
   n <- nmr_baseline_threshold (dataset)
   expect_true(is.numeric(n))
 })
-  
