7b11ebcfe47aeb861a850b88720afbb2567e084a
Hector Gracia <hgracia@ibecbarcelona.eu>
2020-05-22 13:00:29 +0200

    Changes for pass biocCheck ignoring .git, .github excluding rDholping and MUVR related functions defunct nmr_diagnose

--- R/nmr_diagnose.R
+++ R/nmr_diagnose.R
@@ -1,12 +1,4 @@
-#' Deprecated function
-#' @param samples An nmr_dataset
-#' @seealso nmr_normalize
-#' @export
-#' @return normalized nmr
-#' @examples 
-#' nmr_dataset <- nmr_dataset_load(system.file("extdata", "nmr_dataset.rds", package = "AlpsNMR"))
-#' nmr_diagnose(nmr_dataset)
-nmr_diagnose <- function(samples) {
-    .Deprecated("nmr_normalize_extra_info")
-    nmr_normalize_extra_info(samples)
+#' Defunct function
+nmr_diagnose <- function() {
+    .Defunct("nmr_normalize_extra_info")
 }
