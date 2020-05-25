0fa1b1ed41b5b1fecca27c4b57671bd17b1a6f4c
Hector Gracia <hgracia@ibecbarcelona.eu>
2020-05-25 15:54:56 +0200

    to_ASICS remobed from namespace, docs updated

--- R/nmr_diagnose.R
+++ R/nmr_diagnose.R
@@ -1,6 +1,6 @@
 #' Defunct function
 #' @name nmr_diagnose-defunct
-#' @return NULL
+#' @return nmr_normalize_extra_info will return a plot of the info
 #' @keywords internal
 nmr_diagnose <- function() {
     .Defunct("nmr_normalize_extra_info")
