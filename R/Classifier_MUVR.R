add90c93680c0eb9e2040b138fa392e77474fbf5
Héctor <47814702+Hgracia@users.noreply.github.com>
2020-06-15 11:28:57 +0200

    Documentation update and random_subsampling exampled modified to avoid fail in check

--- R/Classifier_MUVR.R
+++ R/Classifier_MUVR.R
@@ -54,7 +54,23 @@ ppm_VIP_vector <- function(VIPs) {
 
 #' Deprecated function
 #' @seealso nmr_data_analysis
+#' Feature selection and validation in multivariate analysis
+#'
+#' Statistical analysis and feature selection in a repeated double
+#' cross-validation frame based on the partial least squares (PLS) or random
+#' forest (RF) analyses using an algorithm for multivariate modelling with
+#' minimally biased variable selection (MUVR) from the `MUVR` package. If your
+#' work with a `nmr_peak_table` object from AlpsNMR, first you need to extract
+#' the X data from the main nmr_dataset object (e.g. your peak table) with the
+#' `nmr_data` function, otherwise you would try to set a list on the X. You also
+#' need to set the class from this object, or just set it from another Y vector.
+#'
+#' @family nmr_dataset_1D functions
+#' @inheritParams MUVR::MUVR
+#'
 #' @export
+#' @return a MUVR model containing selection parameters, validation and fitness
+#' @references Shi,L. et al. (2018) Variable selection and validation in multivariate modelling. Bioinformatics.
 rdCV_PLS_RF <- function(X,
                         Y,
                         ID,
@@ -77,6 +93,16 @@ rdCV_PLS_RF <- function(X,
 
 
 #' Deprecated function
+#' Model plot
+#'
+#' Plot the model (a `MUVR` object) obtained from `rdCV_PLS_RF` function. For
+#' more information about the multivariate modelling with minimally biased
+#' variable selection (MUVR) from the `MUVR` package, see Shi et al., 2018 (DOI:
+#' 10.1093/bioinformatics/bty710).
+#'
+#' @inheritParams MUVR::plotMV
+#'
+#' @return A plot with the model performance
 #' @export
 MUVR_model_plot = function (MVObj,
                             model = "mid",
@@ -87,25 +113,73 @@ MUVR_model_plot = function (MVObj,
 }
 
 #' Deprecated function
+#' Model VIP values
+#'
+#' Once, the MVObj is created and validated, this function extracts autoselected
+#' ranked variables from the model (MUVR object). See `rdCV_PLS_RF` function.
+#'
+#' @inheritParams MUVR::getVIP
+#' @param MVObj a MUVR model
+#' @return a data frame with the order, name and average rank of selected variables
 #' @export
 model_VIP = function(MVObj, model = "mid") {
     .Defunct()
 }   
 
 #' Deprecated function
+#' p-Value from permutation test
+#'
+#' The fucntion calculates the cumulative (1-tailed) probability of 'actual'
+#' belonging to 'h0' (`permutation_object` from the `permutation_test_model` function).
+#'
+#' @param model_actual The actual model performance    (e.g. misclassifications or Q2)
+#' @param permutation_object Null hypothesis distribution from permutation test
+#'     from `permutation_test_model` function
+#'
+#' @return The p-value indicating if there is significant differences between
+#'     the model performance and the null hypothesis distribution from permutation
+#'     test test
 #' @export
 p_value_perm = function (model_actual, permutation_object) {
     .Defunct()
 }   
 
 #' Deprecated function
+#' Confusion matrix of the MUVR model
+#'
+#' After creating a model with the `rdCV_PLS_RF` function, you can run
+#' `confusion_matrix` on the model to make a confusion matrix from MUVR. This
+#' gives information about the model performance (e.g. classification rate).
+#'
+#' @inheritParams MUVR::confusionMatrix
+#'
+#' @return A confusion matrix of the model comparing actual vs predicted class
 #' @export
 confusion_matrix = function(MVObj, model = "mid") {
     .Defunct()
 }   
 
 #' Deprecated function
+#' Feature selection and validation in MULTILEVEL analysis
+#'
+#' Statistical analysis and feature selection in a repeated double
+#' cross-validation frame based on the partial least squares
+#' (PLS) or random forest (RF) analyses using an algorithm
+#' for multivariate modelling with minimally biased variable
+#' selection (MUVR) from the `MUVR` package. The function
+#' `rdCV_PLS_RF_ML` allows the multilevel comparison,
+#' especially useful in crossover or longitudinal studies
+#' (2 timepoints) considering the same individual (it
+#' requires 2 samples of the same observation).
+#'
+#' @family nmr_dataset_1D functions
+#' @param nmr_peak_table an AlpsNMR integration object (2 classes)
+#' @param label the name of the variable to test (e.g. "Timepoint")
+#' @inheritParams MUVR::MUVR
+#'
 #' @export
+#' @return a MUVR model containing selection parameters, validation and fitness
+#' @references Shi,L. et al. (2018) Variable selection and validation in multivariate modelling. Bioinformatics.
 rdCV_PLS_RF_ML = function (nmr_peak_table,
                            label,
                            scale = TRUE,
@@ -124,6 +198,13 @@ rdCV_PLS_RF_ML = function (nmr_peak_table,
 }   
 
 #' Deprecated function
+#' Extracts AUC value
+#'
+#' The function extracts the AUC value from the middle `MUVR` model
+#'
+#' @param MVObj a MUVR model
+#'
+#' @return the AUC value of the middle model
 #' @export
 AUC_model <- function (MVObj) {
     .Defunct()
