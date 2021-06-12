a78067f25156dfd69dc5254bf69e5de05e7060af
Sergio Oller <sergioller@gmail.com>
2021-06-12 08:32:26 +0200

    Test for unique NMRExperiment
    Test for unique NMRExperiment values

--- tests/testthat/test-read_bruker.R
+++ tests/testthat/test-read_bruker.R
@@ -5,3 +5,14 @@ test_that("nmr_read_samples_dir works", {
   dataset <- nmr_read_samples_dir(dir_to_demo_dataset)
   expect_equal(dataset$num_samples, 3)
 })
+
+test_that("nmr_read_samples returns unique NMR experiments", {
+  dir_to_demo_dataset <- system.file("dataset-demo", package = "AlpsNMR")
+  dataset <- nmr_read_samples(
+    c(file.path(dir_to_demo_dataset, "10.zip"),
+      file.path(dir_to_demo_dataset, "10.zip"))
+  )
+  expect_equal(dataset$num_samples, 2)
+  expect_false(duplicated(nmr_meta_get_column(dataset, "NMRExperiment")))
+})
+
