62bab611118f7c26d2215c44b603821712ca49e4
Sergio Oller <sergioller@gmail.com>
2021-08-03 04:47:49 +0200

    Improve the to_Chemospec test

--- tests/testthat/test-to_ChemoSpec.R
+++ tests/testthat/test-to_ChemoSpec.R
@@ -14,7 +14,11 @@ test_that("to_ChemoSpec works with a group", {
   dir_to_demo_dataset <- system.file("dataset-demo", package = "AlpsNMR")
   dataset <- nmr_read_samples_dir(dir_to_demo_dataset)
   dataset <- nmr_interpolate_1D(dataset, axis = c(min = 1, max = 2, by = 0.002))
-  dataset$metadata$external$treatment <- c("A", "B", "A")
+  extra_metadata <- data.frame(
+    NMRExperiment = c("10", "20", "30"),
+    treatment = c("A", "B", "A")
+  )
+  dataset <- nmr_meta_add(dataset, extra_metadata)
   Ch <- to_ChemoSpec(dataset, group = "treatment")
   expect_true(is.list(Ch))
   xx <- ChemoSpec::sumGroups(Ch)
