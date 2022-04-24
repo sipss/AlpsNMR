a949106c002ac36b5028e2a0615c1b8a4dfbebb3
Sergio Oller <sergioller@gmail.com>
2022-04-24 12:57:45 +0200

    Documentation changes to lorentzian fitting functions

--- R/area_estimation.R
+++ R/area_estimation.R
@@ -1,115 +1,121 @@
-#' Peak width estimation for integration
+
+#' Given a list of inflection points, find the ones closer to a peak apex.
 #'
-#' Estimates the peak width (ppm width) to perform peak integration using
-#' `nmr_integrate_peak_positions`. for this purpose, the full width at half maximum
-#' of a peak from alanine doublet is considered.
+#' @param peak_limit_left The indices of all possible left inflection points
+#' @param peak_limit_right  The indices of all possible right inflection points
+#' @param pos The index of the apex we are looking at
 #'
-#' @family peak integration functions
-#' @family nmr_dataset_1D functions
-#' @param nmr_dataset An [nmr_dataset_1D].
-#' @return Numerical. A peak width (ppm) that may be set in `nmr_integrate_peak_positions`
-
-library(AlpsNMR)
-library(gridExtra)
-library(ggplot2)
-library(plotly)
-library(GGally)
-
-
-get_peak_bounds <- function(up, down, length_sgf, pos){
-  up <- up[up - pos > 0]
-  down <- down[pos - down > 0]
-  if(length(up) == 0 || length(down) == 0){
-    return(c("left" = 0, "right" = 0))
-  }
-  right <- up[1]
-  left <- down[length(down)]
-  numr <- right - pos
-  numl <- pos - left 
-  return(c("left" = left, "right" = right))
-}
-
-lorentzian <- function(x, x0, gamma, A) {
-  A * (1/(pi*gamma)) * ((gamma^2) / ((x-x0)^2 + (gamma)^2))
-}
-
-
-fitting <- function(estimated_peak, d, l, r, pos, ymaxi){
-  ms <- sum(
-    (d$y[l:r] - (estimated_peak[l:r] + ymaxi - estimated_peak[pos]))**2
-  )/(r - l + 1)
-  rms <- sqrt(ms)
-  norm_rmse <- rms/estimated_peak[pos]
-  return(norm_rmse)
-}
-
-if (file.exists("/home/laura/IBEC/AlpsNMR-Tutorial/results/07-alignment/peak_data_bo.rds")) {
-  nmr_dataset <- readRDS("/home/laura/IBEC/AlpsNMR-Tutorial/results/07-alignment/nmr_dataset.rds")
-  peak_data_bo <- readRDS("/home/laura/IBEC/AlpsNMR-Tutorial/results/07-alignment/peak_data_bo.rds")
-} else {
-  nmr_dataset <- readRDS("/home/laura/IBEC/AlpsNMR-Tutorial/results/06-filter-samples/nmr_dataset.rds")
-  peak_data <- nmr_detect_peaks(nmr_dataset,
-                                nDivRange_ppm = 0.1,
-                                scales = seq(1, 16, 2),
-                                baselineThresh = NULL,
-                                SNR.Th = 3)
-  message("Choosing alignment reference...")
-  NMRExp_ref <- nmr_align_find_ref(nmr_dataset, peak_data)
-  message("Starting alignment...")
-  nmr_dataset <- nmr_align(nmr_dataset, peak_data,
-                           NMRExp_ref = NMRExp_ref,
-                           maxShift_ppm = 0.0015,
-                           acceptLostPeak = FALSE)
-  peak_data <- nmr_detect_peaks(nmr_dataset,
-                                nDivRange_ppm = 0.1,
-                                scales = seq(1, 16, 2),
-                                baselineThresh = NULL,
-                                SNR.Th = 3)
-  peak_data_bo <- peak_data
-  saveRDS(nmr_dataset, "/home/laura/IBEC/AlpsNMR-Tutorial/results/07-alignment/nmr_dataset.rds")
-  saveRDS(peak_data_bo, "/home/laura/IBEC/AlpsNMR-Tutorial/results/07-alignment/peak_data_bo.rds")
-  rm(peak_data, NMRExp_ref)
-}
-
-
-peak_data <- peak_data_bo
-area_estimation <- function(peak_data, error){
-  peak_data$gamma <- NA_real_
-  peak_data$area <- NA_real_
-  peak_data$norm_rmse <- NA_real_
-  peak_data$accepted <- NA
-  
-  sindex_anterior <- -1
-  for (i in 1:nrow(peak_data)){
-    xmaxi <- peak_data$ppm[i]
-    ymaxi <- peak_data$intensity[i]
-    posi <- peak_data$pos[i]
-    sindex <- peak_data$sample_idx[i]
-    if (sindex != sindex_anterior) {
-      d2 <- data.frame(x = nmr_dataset$axis, y = nmr_dataset$data_1r[sindex,])
-      sgf <- signal::sgolayfilt(d2$y,p=2, n=21, m = 2, ts=d2$x[2]-d2$x[1]) #segona derivada
-      up <- which((diff(sign(sgf)))== 2)
-      down <- which((diff(sign(sgf)))== -2)
+#' @return a named numeric vector with the indices for the left inflection point, the apex and the right inflection point.
+#' @noRd
+get_peak_bounds <- function(peak_limit_left, peak_limit_right, pos){
+    peak_limit_left <- peak_limit_left[pos - peak_limit_left > 0]
+    peak_limit_right <- peak_limit_right[peak_limit_right - pos > 0]
+    if(length(peak_limit_right) == 0 || length(peak_limit_left) == 0){
+        return(c("left" = 0, "apex" = pos, "right" = 0))
     }
-    out2 <- get_peak_bounds(up, down, length(sgf), posi)
-    gamma <- sqrt(3)/2 * (d2$x[out2["right"]]-d2$x[out2["left"]])
-    estimated_A <- - (pi * gamma^3 * sgf[posi])/2
-    estimated_peak <- lorentzian(
-      x = d2$x,
-      x0 = xmaxi,
-      gamma = gamma,
-      A = estimated_A
-    )
-    norm_rmse <- fitting(estimated_peak, d2, out2["left"], out2["right"], posi, ymaxi)
-    peak_data$gamma[i] <- gamma
-    peak_data$area[i] <- estimated_A
-    peak_data$norm_rmse[i] <- norm_rmse
-    if (norm_rmse < error & norm_rmse > 0){ peak_data$accepted[i] <- TRUE }else{peak_data$accepted[i] <- FALSE}
-    sindex_anterior <- sindex
-  }
-  return(peak_data)
+    right <- peak_limit_right[1]
+    left <- peak_limit_left[length(peak_limit_left)]
+    return(c("left" = left, "apex" = pos, "right" = right))
 }
 
 
-peak_data <- area_estimation(peak_data, 0.5)
+#' A lorentzian function estimation
+#' 
+#' \eqn{f(x, x_0, \gamma, A) = A \frac{1}{\pi \gamma} \frac{\gamma^2}{(x-x_0)^2 + \gamma^2}}
+#'
+#' @param x Numbers where the function will be evaluated.
+#' @param x0 The peak location
+#' @param gamma Lorentzian peak width
+#' @param A A multiplication factor. When \eqn{x = x_0}, the lorentzian will be \eqn{\frac{A}{\pi \gamma}}
+#'
+#' @return A numeric vector of the same length than `x`, with the values of the lorentzian
+#' @noRd
+lorentzian <- function(x, x0, gamma, A) {
+    A * (1/(pi*gamma)) * ((gamma^2) / ((x-x0)^2 + gamma^2))
+}
+
+
+get_norm_rmse <- function(y_fitted, y, y_fitted_apex, y_apex){
+    y_fitted_adj <- y_fitted - y_fitted_apex + y_apex
+    mse <- sum((y - y_fitted_adj)**2)/length(y)
+    rmse <- sqrt(mse)
+    norm_rmse <- rmse/y_fitted_apex
+    return(norm_rmse)
+}
+
+
+fit_lorentzians_to_peak_data <- function(peak_data, nmr_dataset, nrmse_max = Inf, area_min = 0, area_max = Inf, ppm_min = 0.1, ppm_max = 12) {
+    peak_data$gamma <- NA_real_
+    peak_data$area <- NA_real_
+    peak_data$norm_rmse <- NA_real_
+    peak_data$accepted <- NA
+    
+    
+    x <- nmr_dataset$axis
+    sindex_prev <- -1
+    # For each peak:
+    for (i in seq_len(nrow(peak_data))) {
+        posi <- peak_data$pos[i]
+        sindex <- peak_data$sample_idx[i]
+        # We want to estimate a lorentzian in a computationally fast way.
+        # peak_data is sorted by sample, so we avoid recalculating.
+        if (sindex != sindex_prev) {
+            y <- nmr_dataset$data_1r[sindex,]
+            sgf <- signal::sgolayfilt(y,p=2, n=21, m = 2, ts=x[2]-x[1]) # 2nd derivative
+            peak_limit_left <- which((diff(sign(sgf)))== -2)
+            peak_limit_right <- which((diff(sign(sgf)))== 2)
+        }
+        peak_bounds <- get_peak_bounds(peak_limit_left, peak_limit_right, posi)
+        # Estimate gamma with the inflection points:
+        # The lorentzian second derivative:
+        # $$f''(x, x_0, A, \gamma) = -\frac{2A \gamma \left(\gamma^2-3\left(x-x_0\right)^2\right)}{\pi \left(\gamma^2+\left(x-x_0\right)^2\right)^3}$$
+        # The inflection points are at:
+        # f''(x, x_0, A, \gamma) = 0 \rightarrow
+        #    x_{right}=x_0+\frac{\sqrt{3}\gamma}{3}
+        #    x_{left}=x_0-\frac{\sqrt{3}\gamma}{3}
+        # Therefore gamma is:
+        # \gamma = \frac{\sqrt{3}}{2} (x_{right} - x_{left} )
+        # FIXME: Possible minor improvement. Use a linear interpolation to
+        #        determine x[peak_bounds["right"]] and the corresponding left.
+        gamma <- sqrt(3)/2 * (x[peak_bounds["right"]] - x[peak_bounds["left"]])
+        # Estimate the amplitude of the lorentzian with the amplitude of the second derivative. 
+        # This has the effect of removing the baseline up to linear effects.
+        # At $x = x_0$, the second derivative is:
+        # f''(x = x_0, x_0, A, \gamma) = -\frac{2A}{\pi \gamma^3}
+        # So we can estimate A as follows:
+        estimated_A <- - (pi * gamma^3 * sgf[posi])/2
+        # And then get the whole lorentzian:
+        y_fitted <- lorentzian(
+            x = x[peak_bounds["left"]:peak_bounds["right"]],
+            x0 = x[peak_bounds["apex"]],
+            gamma = gamma,
+            A = estimated_A
+        )
+        # So we can estimate how good our fit is to the real data:
+        y_fitted_apex_idx <- peak_bounds["apex"] - peak_bounds["left"] + 1L
+        norm_rmse <- get_norm_rmse(
+            y_fitted,
+            y[peak_bounds["left"]:peak_bounds["right"]],
+            y_fitted_apex = y_fitted[y_fitted_apex_idx],
+            y_fitted = y[peak_bounds["apex"]]
+        )
+        # And save the result in the peak_data table:
+        peak_data$gamma[i] <- gamma
+        peak_data$area[i] <- estimated_A
+        peak_data$norm_rmse[i] <- norm_rmse
+        peak_data$accepted[i] <- (
+            norm_rmse <= nrmse_max &&
+                estimated_A >= area_min &&
+                estimated_A <= area_max &&
+                x[peak_bounds["apex"]] >= ppm_min &&
+                x[peak_bounds["apex"]] <= ppm_max &&
+                !is_ppm_region_excluded(
+                    region = c(x[peak_bounds["left"]], x[peak_bounds["right"]]),
+                    nmr_get_excluded_regions(nmr_dataset)
+                )
+        )
+        sindex_prev <- sindex
+    }
+    return(peak_data)
+}
 
