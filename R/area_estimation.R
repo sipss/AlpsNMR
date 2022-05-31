2a13b8bb93fa0a306b102ffb7b54592190370061
Sergio Oller <sergioller@gmail.com>
2022-05-31 08:16:28 +0200

    Document peaklist_fit_lorentzians(). This is an experimental function

--- R/area_estimation.R
+++ R/area_estimation.R
@@ -61,28 +61,141 @@ get_norm_rmse <- function(y_fitted, y, y_fitted_apex, y_apex){
 }
 
 
-peaklist_fit_lorentzians <- function(peak_data, nmr_dataset) {
+refine_lorentzian_fit_with_nls <- function(data_to_fit, start, method) {
+    estimated_A <- start[["A"]]
+    gamma <- start[["gamma"]]
+    peak_pos_ppm <- start[["x0"]]
+    error_msgs <- c()
+    
+    if (method == "peak") {
+        formula <- y ~ A * (1/(pi*gamma)) * ((gamma^2) / ((x-x0)^2 + gamma^2))
+    } else if (method == "2nd_derivative") {
+        formula <- y ~ -2*A*gamma/pi * (gamma^2 - 3*(x-x0)^2) / ( gamma^2 + (x-x0)^2)^3
+    } else {
+        rlang::abort("Unknown method")
+    }
+    
+    tryCatch({
+        mm.model.nls <- stats::nls(
+            formula = formula,
+            data = data_to_fit,
+            start = start
+        )
+        gamma <- mm.model.nls$m$getAllPars()["gamma"]
+        estimated_A <- mm.model.nls$m$getAllPars()["A"]
+        if (gamma < 0 && estimated_A < 0) {
+            gamma <- -gamma
+            estimated_A <- -estimated_A
+        }
+        if (abs(start[["x0"]] - mm.model.nls$m$getAllPars()["x0"]) > gamma) {
+            error_msgs <- c(error_msgs, "peak position moved more than gamma!")
+        }
+        peak_pos_ppm <- mm.model.nls$m$getAllPars()["x0"]
+        
+    }, error = function(e) {
+        msg <- conditionMessage(e)
+        error_msgs <- c(error_msgs, msg)
+    })
+    list(
+        estimated_A = estimated_A,
+        gamma = gamma,
+        peak_pos_ppm = peak_pos_ppm,
+        error_msgs = error_msgs
+    )
+}
+
+
+
+#' Fit lorentzians to each peak to estimate areas
+#' 
+#' The different methods are available for benchmarking while developing, we should pick one.
+#' 
+#' - $\gamma$ is estimated using the inflection points of the signal and fitting them to the lorentzian inflection points
+#' - $A$ is estimated using the `amplitude_method` below
+#' - The peak position ($x_0$) is given in `peak_data`
+#' 
+#' Those estimations may be refined with non-linear least squares using `refine_peak_model`. If the nls does not converge,
+#' the initial estimations are kept. Convergence -and other nls errors- are saved for further reference and diagnostic.
+#' Use `attr(peak_data_fitted, "errors")` to retreive the error messages, where `peak_data_fitted` is assumed to be the
+#' output of this function. The refining improves $\gamma$, $A$ and $x_0$.
+#' 
+#' The baseline estimation (when calculated, see the arguments) is set to Asymmetric Least Squares with 
+#' lambda = 6, p=0.05, maxit=20 and it is probably not optimal... yet.
+#'
+#'
+#'
+#' @param peak_data The peak data
+#' @param nmr_dataset The nmr_dataset object with the data. This function for now assumes nmr_dataset is NOT be baseline corrected
+#' @param amplitude_method The method to estimate the amplitude. It may be:
+#'  - `"intensity"`. The amplitude of the peak is proportional to the raw intensity at the apex. This is a bad estimation if
+#'    the intensity includes a baseline, because the amplitude of the peak will be overestimated
+#'  - `"2nd_derivative"`: The amplitude of the peak is proportional to the second derivative of the raw intensity signal at the apex.
+#'    This method aims to correct the "intensity" method, since it is expected that the baseline will be mostly removed
+#'    when considering the 2nd derivative of the spectrum. The 2nd derivative is calculated with a 2nd order Savitzky-Golay filter of 21 points.
+#'  - `"intensity_without_baseline"`: A baseline is estimated on the whole spectra and subtracted from it. Then the peak amplitude
+#'    is proportional to the corrected intensity at the apex (as in the "intensity" method).
+#' @param refine_peak_model. Whether a non linear least squares fitting should be used to refine the estimated parameters. It can be:
+#'  - `"none"`: Do not refine using nls.
+#'  - `"peak"`: Use a lorentzian peak model and the baseline corrected spectra.
+#'  - `"2nd_derivative"`:
+#'
+#'
+#' 
+#'
+#' @return The given data frame `peak_data`, with added columns:
+#'  - inflection points, 
+#'  - gamma
+#'  - area 
+#'  - a norm_rmse fitting error
+#'  
+#'  As well as some attributes
+#'  - "errors": A data frame with any error in the peak fitting
+#'  - "fit_baseline": Whether the method used has any consideration for the baseline of the signal (maybe not very useful attribute)
+#'  - "method_description": A textual description of what we did, to include it in plots
+#'  @export
+peaklist_fit_lorentzians <- function(
+        peak_data, 
+        nmr_dataset, 
+        amplitude_method = c("intensity", "2nd_derivative", "intensity_without_baseline"),
+        refine_peak_model = c("none", "peak", "2nd_derivative")) {
     peak_data$ppm_infl_min <- NA_real_
     peak_data$ppm_infl_max <- NA_real_
     peak_data$gamma_ppb <- NA_real_
     peak_data$area <- NA_real_
     peak_data$norm_rmse <- NA_real_
+    amplitude_method <- match.arg(amplitude_method)
+    refine_peak_model <- match.arg(refine_peak_model)
+    method_description <- c()
     
     x <- nmr_dataset$axis
     sindex_prev <- -1
+    all_errors <- list(peak_id = character(0L), error_msg = character(0L))
+    pb <- progress_bar_new(name = "Fitting lorentzians", total = nrow(peak_data))
+
     # For each peak:
     for (i in seq_len(nrow(peak_data))) {
+        progress_bar_update(pb)
         posi <- peak_data$pos[i]
         sindex <- peak_data$sample_idx[i]
+        peak_pos_ppm <- peak_data$ppm[i]
         # We want to estimate a lorentzian in a computationally fast way.
         # peak_data is sorted by sample, so we avoid recalculating.
         if (sindex != sindex_prev) {
             y <- nmr_dataset$data_1r[sindex,]
-            sgf <- signal::sgolayfilt(y,p=2, n=21, m = 2, ts=x[2]-x[1]) # 2nd derivative
-            peak_limit_left <- which((diff(sign(sgf)))== -2)
-            peak_limit_right <- which((diff(sign(sgf)))== 2)
+            if (amplitude_method == "intensity_without_baseline" || refine_peak_model == "peak") {
+                y_basel <- baseline::baseline.als(
+                    spectra = nmr_dataset$data_1r[sindex,, drop = FALSE],
+                    lambda = 6,
+                    p = 0.05,
+                    maxit = 20
+                )$baseline |> as.numeric()
+                y_nobasel <- y - y_basel
+            }
+            sgf <- signal::sgolayfilt(y, p = 2, n = 21, m = 2, ts = x[2] - x[1]) # 2nd derivative
+            peak_limit_left <- which((diff(sign(sgf))) == -2)
+            peak_limit_right <- which((diff(sign(sgf))) == 2)
         }
-        peak_bounds <- get_peak_bounds(peak_limit_left, peak_limit_right, posi, x, sgf)
+        peak_bounds <- AlpsNMR:::get_peak_bounds(peak_limit_left, peak_limit_right, posi, x, sgf)
         # Estimate gamma with the inflection points:
         # The lorentzian second derivative:
         # $$f''(x, x_0, A, \gamma) = -\frac{2A \gamma \left(\gamma^2-3\left(x-x_0\right)^2\right)}{\pi \left(\gamma^2+\left(x-x_0\right)^2\right)^3}$$
@@ -93,12 +206,67 @@ peaklist_fit_lorentzians <- function(peak_data, nmr_dataset) {
         # Therefore gamma is:
         # \gamma = \frac{\sqrt{3}}{2} (x_{right} - x_{left} )
         gamma <- sqrt(3)/2 * (peak_bounds["xright"] - peak_bounds["xleft"])
-        # Estimate the amplitude of the lorentzian with the amplitude of the second derivative. 
-        # This has the effect of removing the baseline up to linear effects.
-        # At $x = x_0$, the second derivative is:
-        # f''(x = x_0, x_0, A, \gamma) = -\frac{2A}{\pi \gamma^3}
-        # So we can estimate A as follows:
-        estimated_A <- - (pi * gamma^3 * sgf[posi])/2
+        if (amplitude_method == "intensity") {
+            # Estimate the amplitude of the lorentzian with the amplitude of the peak 
+            estimated_A <- pi*gamma*peak_data$intensity[i]
+        }  else if (amplitude_method == "2nd_derivative") {
+            # Estimate the amplitude of the lorentzian with the amplitude of the second derivative. 
+            # This has the effect of removing the baseline up to linear effects.
+            # At $x = x_0$, the second derivative is:
+            # f''(x = x_0, x_0, A, \gamma) = -\frac{2A}{\pi \gamma^3}
+            # So we can estimate A as follows:
+            estimated_A <- -(pi * gamma^3 * sgf[posi])/2
+        } else if (amplitude_method == "intensity_without_baseline") {
+            # Estimate A based on the amplitude of the signal without baseline
+            estimated_A <- y_nobasel[posi]*pi*gamma
+        } else {
+            rlang::abort(sprintf("amplitude_method '%s'unknown", amplitude_method))
+        }
+        if (identical(refine_peak_model, "peak")) {
+            # Further fitting with nls:
+            new_params <- refine_lorentzian_fit_with_nls(
+                data_to_fit = data.frame(
+                    x = x[peak_bounds["left"]:peak_bounds["right"]],
+                    y = y_nobasel[peak_bounds["left"]:peak_bounds["right"]]
+                ),
+                start = list(
+                    A = estimated_A,
+                    x0 = x[peak_bounds["apex"]],
+                    gamma = gamma
+                ),
+                method = refine_peak_model
+            )
+            gamma <- new_params[["gamma"]]
+            estimated_A <- new_params[["estimated_A"]]
+            peak_pos_ppm <- new_params[["peak_pos_ppm"]]
+            if (!is.null(new_params[["error_msgs"]])) {
+                all_errors$peak_id <- c(all_errors$peak_id, peak_data$peak_id[i])
+                all_errors$error_msg <- paste(new_params[["error_msgs"]], collapse = "\n")
+            }
+        } else if (identical(refine_peak_model, "2nd_derivative")) {
+            # Further fitting with nls:
+            new_params <- refine_lorentzian_fit_with_nls(
+                data_to_fit = data.frame(
+                    x = x[peak_bounds["left"]:peak_bounds["right"]],
+                    y = sgf[peak_bounds["left"]:peak_bounds["right"]]
+                ),
+                start = list(
+                    A = estimated_A,
+                    x0 = x[peak_bounds["apex"]],
+                    gamma = gamma
+                ),
+                method = refine_peak_model
+            )
+            gamma <- new_params[["gamma"]]
+            estimated_A <- new_params[["estimated_A"]]
+            peak_pos_ppm <- new_params[["peak_pos_ppm"]]
+            if (!is.null(new_params[["error_msgs"]])) {
+                all_errors$peak_id <- c(all_errors$peak_id, peak_data$peak_id[i])
+                all_errors$error_msg <- paste(new_params[["error_msgs"]], collapse = "\n")
+            }
+        } else if (!identical(refine_peak_model, "none")) {
+            rlang::abort("Unknown refine_peak_model")
+        }
         # And then get the whole lorentzian:
         y_fitted <- lorentzian(
             x = x[peak_bounds["left"]:peak_bounds["right"]],
@@ -122,8 +290,62 @@ peaklist_fit_lorentzians <- function(peak_data, nmr_dataset) {
         peak_data$norm_rmse[i] <- norm_rmse
         sindex_prev <- sindex
     }
+    
+    # Method description (useful for plotting)
+    method_description <- c(method_description, "Estimate \u03b3 with inflection points")
+    if (amplitude_method == "intensity") {
+        fits_on_baseline <- FALSE
+        method_description <- c(
+            method_description,
+            "Estimate A with raw peak intensity",
+            "May have baseline issues"
+        )
+    }  else if (amplitude_method == "2nd_derivative") {
+        fits_on_baseline <- TRUE
+        method_description <- c(
+            method_description,
+            "Estimate A with second derivative",
+            "2nd derivative should remove baseline"
+        )
+    } else if (amplitude_method == "intensity_without_baseline") {
+        fits_on_baseline <- TRUE
+        method_description <- c(
+            method_description,
+            "Remove baseline using ALS",
+            "Estimate A with the amplitude of the baseline-corrected signal"
+        )
+        
+    }
+    
+    if (identical(refine_peak_model, "peak")) {
+        method_description <- c(
+            method_description,
+            "Refine \u03b3, A and x0 (nls fits a lorentzian)"
+        )
+    } else if (identical(refine_peak_model, "2nd_derivative")) {
+        method_description <- c(
+            method_description,
+            "Refine \u03b3, A and x0 (nls fits a lorentzian on 2nd deriv)"
+        )
+    } else if (identical(refine_peak_model, "none")) {
+        method_description <- c(
+            method_description,
+            "nls refining disabled"
+        )
+    }
+    
+    
+    
+    attr(peak_data, "errors") <- as.data.frame(all_errors)
+    attr(peak_data, "fits_on_baseline") <- fits_on_baseline
+    attr(peak_data, "method_description") <- paste(
+        method_description,
+        collapse = "\n"
+    )
     peak_data
 }
+
+
 
 
 #' Peak list: Create an `accepted` column based on some criteria
