d3c0b3474a78d107b9f568aa204518bb653dcb0b
Sergio Oller <sergioller@gmail.com>
2022-06-17 08:05:28 +0200

    Baseline estimation and removal

--- R/nmr_baseline_removal.R
+++ R/nmr_baseline_removal.R
@@ -19,13 +19,45 @@ nmr_baseline_removal <- function(nmr_dataset,
                                  lambda = 6,
                                  p = 0.05,
                                  maxit = 20) {
-    results <-
-        baseline::baseline.als(
-            nmr_dataset$data_1r,
-            lambda = lambda,
-            p = p,
-            maxit = maxit
-        )
-    nmr_dataset$data_1r <- results$corrected
+    results <- baseline::baseline(
+        nmr_dataset$data_1r,
+        method = "als",
+        lambda = lambda,
+        p = p,
+        maxit = maxit
+    )
+    nmr_dataset$data_1r <- baseline::getCorrected(results)
     nmr_dataset
 }
+
+
+#' Estimate the baseline on an [nmr_dataset_1D] object, using [baseline::baseline.als].
+#'
+#' @family baseline removal functions
+#' @seealso [baseline::baseline.als]
+#' @param nmr_dataset An [nmr_dataset_1D].
+#' @inheritParams baseline::baseline.als
+#' @return The same [nmr_dataset_1D] object with the `data_1r_baseline` element.
+#' @export
+#'
+#' @examples 
+#' dir_to_demo_dataset <- system.file("dataset-demo", package = "AlpsNMR")
+#' dataset <- nmr_read_samples_dir(dir_to_demo_dataset)
+#' dataset_1D <- nmr_interpolate_1D(dataset, axis = c(min = -0.5, max = 10, by = 2.3E-4))
+#' dataset_1D <- nmr_baseline_estimation(dataset_1D, lambda = 9, p = 0.01)
+#' 
+nmr_baseline_estimation <- function(nmr_dataset,
+                                    lambda = 9,
+                                    p = 0.05,
+                                    maxit = 20) {
+    results <- baseline::baseline(
+        nmr_dataset$data_1r,
+        method = "als",
+        lambda = lambda,
+        p = p,
+        maxit = maxit
+    )
+    nmr_dataset$data_1r_baseline <- baseline::getBaseline(results)
+    nmr_dataset
+}
+
