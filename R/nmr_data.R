df579983d33c41e0c55ac7493d34e3327cc0bfc7
Sergio Oller <sergioller@gmail.com>
2022-06-20 19:53:12 +0200

    nmr_data() accessor accepts a "what" argument to choose the matrix to export

--- R/nmr_data.R
+++ R/nmr_data.R
@@ -15,19 +15,19 @@ nmr_data <- function(nmr_dataset, ...) {
     UseMethod("nmr_data")
 }
 
-#' @noRd
+#' @rdname nmr_data
+#' @param what What data do we want to get (default: `data_1r`)
 #' @export
 #' @examples 
 #' dir_to_demo_dataset <- system.file("dataset-demo", package = "AlpsNMR")
 #' dataset <- nmr_read_samples_dir(dir_to_demo_dataset)
 #' dataset_1D <- nmr_interpolate_1D(dataset, axis = c(min = -0.5, max = 10, by = 2.3E-4))
 #' dataset_1D_data <- nmr_data(dataset_1D)
-nmr_data.nmr_dataset_1D <- function(nmr_dataset, ...) {
-    data_1r <- nmr_dataset$data_1r
-    rownames(data_1r) <-
-        nmr_meta_get_column(nmr_dataset, "NMRExperiment")
-    colnames(data_1r) <- nmr_dataset$axis
-    data_1r
+nmr_data.nmr_dataset_1D <- function(nmr_dataset, what = "data_1r", ...) {
+    mat <- nmr_dataset[[what]]
+    rownames(mat) <- names(nmr_dataset)
+    colnames(mat) <- nmr_dataset$axis
+    mat
 }
 
 #' @noRd
@@ -39,27 +39,29 @@ nmr_data.nmr_dataset_1D <- function(nmr_dataset, ...) {
 #' peak_table <- nmr_data(dataset_1D)
 nmr_data.nmr_dataset_peak_table <- function(nmr_dataset, ...) {
     peak_table <- nmr_dataset$peak_table
-    rownames(peak_table) <-
-        nmr_meta_get_column(nmr_dataset, "NMRExperiment")
+    rownames(peak_table) <- names(nmr_dataset)
     peak_table
 }
 
 
 #' @rdname nmr_data
 #' @param value A matrix
+#' @param ... Passed on to methods for compatibility
 #'
 #' @return The given nmr_dataset
 #' @export
-"nmr_data<-" <- function(nmr_dataset, value) {
+"nmr_data<-" <- function(nmr_dataset, ..., value) {
     UseMethod("nmr_data<-")
 }
 
 #' @rdname nmr_data
 #' @export
-"nmr_data<-.nmr_dataset_1D" <- function(nmr_dataset, value) {
-    value <- as.matrix(value)
-    stopifnot(nrow(value) == nmr_dataset$num_samples)
-    rownames(value) <- colnames(value) <- NULL
-    nmr_dataset$data_1r <- value
+"nmr_data<-.nmr_dataset_1D" <- function(nmr_dataset, what = "data_1r", ..., value) {
+    if (!is.null(value)) {
+        value <- as.matrix(value)
+        stopifnot(nrow(value) == nmr_dataset$num_samples)
+        rownames(value) <- colnames(value) <- NULL
+    }
+    nmr_dataset[[what]] <- value
     nmr_dataset
 }
