5980e6b09a7c9516715b5cac1191d2a0b5c856b8
Sergio Oller <sergioller@gmail.com>
2022-06-23 12:42:40 +0200

    Normalization accounts for baseline estimation and gives normalized normalization factors to prevent introducing global scaling factors

--- R/nmr_normalize.R
+++ R/nmr_normalize.R
@@ -1,22 +1,33 @@
 #' PQN normalization
 #' @noRd
 #' @param spectra A matrix with one spectrum on each row
+#' @param basel A matrix same size as spectra, with the baseline estimation
 #' @return A list with `spectra` (a matrix with one spectrum on each row) and `norm_factor`
-norm_pqn <- function(spectra) {
+norm_pqn <- function(spectra, basel = NULL) {
     num_samples <- nrow(spectra)
     if (num_samples < 10) {
-        warning("The Probabalistic Quotient Normalization requires several samples ",
-                        "to compute the median spectra. Your number of samples is low")
+        rlang::warn(
+            message = c(
+                "There are not enough samples for reliably estimating the median spectra",
+                "i" = paste0(
+                    "The Probabalistic Quotient Normalization requires several samples ",
+                    "to compute the median spectra. Your number of samples is low"
+                ),
+                "i" = paste0("Review your peaks before and after normalization to ",
+                             "ensure there are no big distortions")
+            )
+        )
     }
     # Normalize to the area
-    areas <- rowSums(spectra)
-    spectra2 <- spectra / areas
+    areas <- rowSums(spectra - basel)
+    areas <- areas/stats::median(areas)
     if (num_samples == 1) {
         # We have warned, and here there is nothing to do anymore
-        warning("PQN is absurd with a single sample. We have normalized it to the area.")
-        return(list(spectra = spectra2,
-                                norm_factor = areas))
+        rlang::warn("PQN is meaningless with a single sample. We have normalized it to the area.")
+        return(list(spectra = spectra / areas,
+                    norm_factor = areas))
     }
+    spectra2 <- (spectra - basel) / areas
     # Move spectra above zero:
     if (any(spectra2 < 0)) {
         spectra2 <- spectra2 - min(spectra2)
@@ -27,9 +38,14 @@ norm_pqn <- function(spectra) {
     f <- spectra2/m[col(spectra2)]
     f <- matrixStats::rowMedians(f)
     # Divide each spectra by its f value
-    spectra3 <- spectra2 / f
-    list(spectra = spectra3,
-             norm_factor = f*areas)
+    out <- list(
+        spectra = spectra / (f*areas),
+        norm_factor = f*areas
+    )
+    if (!is.null(basel)) {
+        out$basel <- basel / (f*areas)
+    }
+    out
 }
 
 
@@ -37,6 +53,10 @@ norm_pqn <- function(spectra) {
 #' 
 #' The `nmr_normalize` function is used to normalize all the samples according
 #' to a given criteria.
+#' 
+#' The aim is to correct from changes between samples, so no matter the criteria
+#' used to normalize, once we get the factors (e.g. the areas), we divide them by
+#' the median normalization factor to avoid introducing global scaling factors.
 #' 
 #' The `nmr_normalize_extra_info` function is used to extract additional information
 #' after the normalization. Typically, we want to know what was the actual normalization
@@ -81,11 +101,16 @@ nmr_normalize <- function(samples,
         return(samples)
     }
     dots <- list(...)
-    
+    spec <- samples[["data_1r"]]
+    if ("data_1r_baseline" %in% names(unclass(samples))) {
+        basel <- samples[["data_1r_baseline"]]
+    } else {
+        basel <- NULL
+    }
     if (method == "area") {
-        norm_factor <- rowSums(samples[["data_1r"]])
+        norm_factor <- rowSums(spec - basel)
     } else if (method == "max") {
-        norm_factor <- apply(samples[["data_1r"]], 1, max)
+        norm_factor <- apply(spec - basel, 1, max)
     } else if (method == "value") {
         norm_factor <- dots[["values"]]
     } else if (method == "region") {
@@ -93,18 +118,34 @@ nmr_normalize <- function(samples,
         norm_factor <- nmr_integrate_regions(samples, regions = list(ic = ppm_range), ...)
         norm_factor <- norm_factor$peak_table[, "ic"]
     } else if (method == "pqn") {
-        norm_result <- norm_pqn(samples[["data_1r"]])
+        norm_result <- norm_pqn(spec, basel)
         samples[["data_1r"]] <- norm_result$spectra
+        if (!is.null(basel)) {
+            samples[["data_1r_baseline"]] <- norm_result$basel
+        }
         norm_factor <- norm_result$norm_factor
         samples <- nmr_normalize_add_extra_info(samples, norm_factor)
         return(samples)
     } else {
         stop("Unimplemented method: ", method)
     }
-    samples[["data_1r"]] <- sweep(x = samples[["data_1r"]],
-                                  MARGIN = 1,
-                                  STATS = norm_factor,
-                                  FUN = "/")
+    # Normalize the normalization factors to the median, to avoid large changes
+    norm_factor_to_apply <- norm_factor / stats::median(norm_factor)
+    samples[["data_1r"]] <- sweep(
+        x = samples[["data_1r"]],
+        MARGIN = 1,
+        STATS = norm_factor_to_apply,
+        FUN = "/"
+    )
+    if (!is.null(basel)) {
+        samples[["data_1r_baseline"]] <- sweep(
+            x = samples[["data_1r_baseline"]],
+            MARGIN = 1,
+            STATS = norm_factor_to_apply,
+            FUN = "/"
+        )
+        
+    }
     samples <- nmr_normalize_add_extra_info(samples, norm_factor)
     return(samples)
 }
@@ -121,18 +162,19 @@ nmr_normalize_extra_info <- function(samples) {
 }
 
 nmr_normalize_add_extra_info <- function(samples, norm_factor) {
-    norm_factor_df <- cbind(nmr_meta_get(samples, columns = "NMRExperiment"),
-                                                    norm_factor = norm_factor)
-    
-    norm_factor_df$norm_factor_norm <- norm_factor_df$norm_factor/stats::median(norm_factor_df$norm_factor)
-    
-    # This plot should have the y axis in log2 scale
+    norm_factor_df <- data.frame(
+        NMRExperiment = names(samples),
+        norm_factor = norm_factor,
+        norm_factor_norm = norm_factor / stats::median(norm_factor)
+    )
+
     gplt <- ggplot2::ggplot(norm_factor_df) +
         ggplot2::geom_col(ggplot2::aes(x = .data$NMRExperiment, y = .data$norm_factor_norm)) +
         ggplot2::scale_x_discrete(name = "NMRExperiment", limits = rev(sort(norm_factor_df$NMRExperiment))) +
         ggplot2::geom_hline(yintercept = 1, color = "red") +
         ggplot2::scale_y_continuous(name = "Normalization factors (normalized to the median)") +
-        ggplot2::coord_flip()
+        ggplot2::coord_flip() +
+        ggplot2::labs(caption = "Each sample was divided by the corresponding factor")
     attr(samples, "normalize_extra_info") <- list(
         norm_factor = norm_factor_df,
         plot = gplt
