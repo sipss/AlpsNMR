75f4bb9b716c5c86eff57232c48fcf5a9de7facc
Sergio Oller <sergioller@gmail.com>
2022-06-26 11:57:31 +0200

    Fix download function
    A mess of zip and archive, but it does not segfault anymore.
    
    Paths fixed as well

--- R/download_MTBLS242.R
+++ R/download_MTBLS242.R
@@ -15,7 +15,7 @@
 #' download_MTBLS242("./MTBLS242")
 #' }
 download_MTBLS242 <- function(dest_dir = "MTBLS242", force = FALSE) {
-    require_pkgs(pkg = c("curl", "zip"))
+    require_pkgs(pkg = c("curl", "zip", "archive"))
     url <- "ftp://ftp.ebi.ac.uk/pub/databases/metabolights/studies/public/MTBLS242/"
     
     dir.create(dest_dir, recursive = TRUE, showWarnings = FALSE)
@@ -64,7 +64,8 @@ download_MTBLS242 <- function(dest_dir = "MTBLS242", force = FALSE) {
         sample_annot <- dplyr::group_by(sample_annot, .data$SampleID)
         sample_annot <- dplyr::filter(sample_annot, dplyr::n() == 2)
         sample_annot <- dplyr::ungroup(sample_annot)
-        sample_annot <- dplyr::select(sample_annot, -"timepoint")
+        sample_annot$NMRExperiment <- sample_annot$filename
+        sample_annot <- dplyr::select(sample_annot, -"timepoint", -"filename")
         
         utils::write.table(sample_annot, file = annotations_destfile, sep = "\t", row.names = FALSE)
     } else {
@@ -74,14 +75,14 @@ download_MTBLS242 <- function(dest_dir = "MTBLS242", force = FALSE) {
     rlang::inform("Downloading samples and keeping CPMG spectra, please wait...")
     pb <- progress_bar_new(
         name = "Preparing samples",
-        total = length(sample_annot$filename)
+        total = length(sample_annot$NMRExperiment)
     )
     dst_rootdir <- file.path(dest_dir, "samples")
     dir.create(dst_rootdir, recursive = TRUE, showWarnings = FALSE)
     purrr::walk(
-        sample_annot$filename,
-        function(filename_base, url, dst_rootdir, keep_only_CPMG_1r, progress_bar = NULL) {
-            progress_bar_update(pb = progress_bar)
+        sample_annot$NMRExperiment,
+        function(filename_base, url, dst_rootdir, keep_only_CPMG_1r) {
+            progress_bar_update(pb)
             filename <- paste0(filename_base, ".zip")
             src_url <- file.path(url, filename)
             final_dst_file <- file.path(dst_rootdir, filename)
@@ -92,6 +93,14 @@ download_MTBLS242 <- function(dest_dir = "MTBLS242", force = FALSE) {
             }
             curl::curl_download(url = src_url, destfile = intermediate_dst_file)
             if (keep_only_CPMG_1r) {
+                # zip file management in R is as of mid 2022 a pain:
+                # Base R provides tools that are not portable (system dependent)
+                # The `zip` package segfaults (to me and to many github issues)
+                # The `archive` package does not provide an API to extract selectively folders from a zip file.
+                # We'll see if the situation improves in some years, so we can clean this mess:
+                # For now, we unzip with zip::unzip() because it is portable, does not segfault and allows
+                # us to extract a subdir.
+                # We will zip with archive because it is portable and does not segfault
                 filenames_in_zip <- zip::zip_list(intermediate_dst_file)[["filename"]]
                 prefix_to_keep <- file.path(filename_base, "3", "") # subdirectory 3/ contains the CPMG measurement
                 filenames_in_zip <- filenames_in_zip[startsWith(filenames_in_zip, prefix_to_keep)]
@@ -105,17 +114,22 @@ download_MTBLS242 <- function(dest_dir = "MTBLS242", force = FALSE) {
                 unlink(tempname, recursive = TRUE)
                 unlink(file.path(filename_base, "fid"))
                 unlink(file.path(filename_base, "pdata", "1", "1i"))
-                utils::zip(zipfile = filename, files = filename_base)
-                setwd(cwd)
-                # zip::zip segfaults
+                # pick your poison:
+                # a) utils::zip() is annoyingly verbose and possibly not portable
+                #utils::zip(zipfile = filename, files = filename_base)
+                #setwd(cwd)
+                # b) zip::zip() segfaults
                 #zip::zip(zipfile = final_dst_file, files = filename_base, root = dst_rootdir)
+                # c) archive (yet another package)
+                archive::archive_write_dir(archive = filename, dir = filename_base)
+                setwd(cwd)
+                # And once you have the zip file, remove the directory:
                 unlink(file.path(dst_rootdir, filename_base), recursive = TRUE)
             }
         },
         url = url,
         dst_rootdir = dst_rootdir,
-        keep_only_CPMG_1r = TRUE,
-        progress_bar = pb
+        keep_only_CPMG_1r = TRUE
     )
     progress_bar_end(pb)
     sample_annot
