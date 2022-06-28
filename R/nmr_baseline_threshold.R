efcf59d045eb2195230a8c44df47e6182fe94aa8
Sergio Oller <sergioller@gmail.com>
2022-06-28 07:03:33 +0200

    The baseline threshold in the peak detection will be dynamic following the baseline

--- R/nmr_baseline_threshold.R
+++ R/nmr_baseline_threshold.R
@@ -25,7 +25,11 @@ nmr_baseline_threshold <- function(nmr_dataset, range_without_peaks = c(9.5, 10)
     threshold_ind <- nmr_dataset$axis >= r_start & nmr_dataset$axis < r_end
     out <- rep(NA_real_, nmr_dataset$num_samples)
     for (i in seq_len(nmr_dataset$num_samples)) {
-        spec_region <- nmr_dataset$data_1r[i, threshold_ind]
+        if ("data_1r_baseline" %in% names(unclass(nmr_dataset))) {
+            spec_region <- nmr_dataset$data_1r[i, threshold_ind] - nmr_dataset$data_1r_baseline[i, threshold_ind]
+        } else {
+            spec_region <- nmr_dataset$data_1r[i, threshold_ind]
+        }
         out[i] <- stats::median(spec_region) + 3*stats::mad(spec_region)
     }
     names(out) <- names(nmr_dataset)
@@ -53,14 +57,79 @@ nmr_baseline_threshold <- function(nmr_dataset, range_without_peaks = c(9.5, 10)
 #' baselineThresh <- nmr_baseline_threshold(dataset_1D)
 #' nmr_baseline_threshold_plot(dataset_1D, baselineThresh)
 nmr_baseline_threshold_plot <- function(nmr_dataset, thresholds, NMRExperiment = "all", chemshift_range = c(9.5, 10), ...) {
-    if (NMRExperiment != "all") {
+    if (is.null(NMRExperiment)) {
+        if (nmr_dataset$num_samples > 20) {
+            NMRExperiment <- sample(names(nmr_dataset), size = 10)
+        } else {
+            NMRExperiment <- names(nmr_dataset)
+        }
+    } else if (identical(NMRExperiment, "all")) {
+        NMRExperiment <- names(nmr_dataset)
+    }
+    if (!identical(NMRExperiment, "all")) {
         thresholds <- thresholds[NMRExperiment]
     }
-    plot(nmr_dataset, chemshift_range = chemshift_range, NMRExperiment = NMRExperiment, ...) +
-        ggplot2::geom_hline(
-            ggplot2::aes(yintercept = .data$threshold),
-            data = tibble::enframe(thresholds, name = "NMRExperiment", value = "threshold")
-        ) +
+    
+    aes_str <- as.character(list(...))
+    columns_to_request <- c("NMRExperiment", get_vars_from_aes_string(aes_str))
+    to_plot <- tidy(
+        nmr_dataset,
+        chemshift_range = chemshift_range,
+        NMRExperiment = NMRExperiment,
+        columns = columns_to_request,
+        matrix_name = "data_1r"
+    )
+    
+    if ("data_1r_baseline" %in% names(unclass(nmr_dataset))) {
+        to_plot_threshold <- tidy(
+            nmr_dataset,
+            chemshift_range = chemshift_range,
+            NMRExperiment = NMRExperiment,
+            columns = columns_to_request,
+            matrix_name = "data_1r_baseline"
+        )
+        to_plot_threshold <- dplyr::left_join(
+            to_plot_threshold,
+            tibble::enframe(
+                thresholds,
+                name = "NMRExperiment",
+                value = "threshold"
+            ),
+            by = "NMRExperiment"
+        )
+        to_plot_threshold$intensity <- to_plot_threshold$intensity + to_plot_threshold$threshold
+    } else {
+        to_plot_threshold <-  dplyr::left_join(
+            to_plot,
+            tibble::enframe(
+                thresholds,
+                name = "NMRExperiment",
+                value = "threshold"
+            ),
+            by = "NMRExperiment"
+        )
+        to_plot_threshold$intensity <- to_plot_threshold$threshold
+    }
+    
+    dotdotdot_aes <- list(...)
+    fixed_aes <- list(
+        x = "chemshift",
+        y = "intensity",
+        group = "NMRExperiment"
+    )
+    all_aes <- c(fixed_aes, dotdotdot_aes)
+    if (!"color" %in% names(all_aes) && !"colour" %in% names(all_aes)) {
+        all_aes <- c(all_aes, list(color = "NMRExperiment"))
+    }
+    ymax <- 1.5*max(to_plot_threshold$intensity)
+
+    gplt <- ggplot2::ggplot() +
+        ggplot2::geom_line(mapping = do.call(ggplot2::aes_string, all_aes), data = to_plot) +
+        ggplot2::geom_line(mapping = do.call(ggplot2::aes_string, all_aes), data = to_plot_threshold, linetype = "dashed", color = "black") +
+        ggplot2::labs(x = "Chemical Shift (ppm)", y = "Intensity (a.u.)") +
+        ggplot2::scale_x_reverse(limits = rev(chemshift_range[seq_len(2)])) +
+        ggplot2::scale_y_continuous(labels = scales::label_number(scale_cut = scales::cut_si("")), limits = c(0, ymax)) +
         ggplot2::facet_wrap(~factor(NMRExperiment, levels = unique(NMRExperiment))) +
         ggplot2::theme(legend.position = "none")
+    gplt
 }
