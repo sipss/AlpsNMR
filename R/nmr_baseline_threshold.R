4eee728a68b8ed04324b22fb8a6d4c94a074952f
Sergio Oller <sergioller@gmail.com>
2022-10-14 11:46:36 +0200

    Make nmr_baseline_threshold backwards compatible with previous version

--- R/nmr_baseline_threshold.R
+++ R/nmr_baseline_threshold.R
@@ -1,11 +1,25 @@
 #' Threshold estimation for peak detection
 #'
-#' Estimates a robust baseline threshold value for peak detection on an [nmr_dataset_1D] object.
-#'    This is performed computing the median and the mean absolute deviation of each spectrum
-#'    in the 9.5-10 ppm range. The threshold is computed as the `median+3*mad`.
+#' Estimates the threshold value for peak detection on an [nmr_dataset_1D] object by examining
+#' a range without peaks, by default the 9.5 - 10 ppm range.
+#' 
+#' Two methods can be used:
+#'
+#' - "mean3sd": The mean3sd method computes the mean and the standard deviation of each spectrum
+#' in the 9.5 - 10 ppm range. The mean spectrum and the mean standard deviation are both vectors
+#' of length equal to the number of points in the given range. The mean of the mean spectrum
+#  and the mean of the standard deviations are used to summarize the center and dispersion of
+#' the noise. The threshold is defined as `center + 3 dispersion`, and it is one single threshold
+#' for the whole dataset. This is the default for backwards compatibility.
+#'
+#' - "median3mad": First we take the data matrix. If we have estimated a baseline already,
+#'   subtract it. In the defined region without peaks, estimate the median of each sample and
+#'   its median absolute deviation. Return a vector of length equal to the number of samples
+#'   with the `median+3mad` for each sample. This is a new more robust method.
 #'
 #' @family peak detection functions
 #' @param nmr_dataset An [nmr_dataset_1D].
+#' @param method Either "mean3sd" or the more robust "median3mad". See the details.
 #' @param range_without_peaks A vector with two doubles describing a range without peaks suitable for baseline detection
 #' @return Numerical. A threshold value in intensity below that no peak is detected.
 #' @export
@@ -15,14 +29,20 @@
 #' dataset_1D <- nmr_interpolate_1D(dataset, axis = c(min = -0.5, max = 10, by = 2.3E-4))
 #' bl_threshold <- nmr_baseline_threshold(dataset_1D)
 #' 
-nmr_baseline_threshold <- function(nmr_dataset, range_without_peaks = c(9.5, 10)) {
+nmr_baseline_threshold <- function(nmr_dataset, range_without_peaks = c(9.5, 10),  method = c("mean3sd", "median3mad")) {
     # FIXME: Maybe a whole baseline would be better, so we can cope with slowly changing baselines better
+    method <- match.arg(method)
     if (length(range_without_peaks) != 2) {
         rlang::abort("range_without_peaks must have length 2")
     }
     r_start <- min(range_without_peaks)
     r_end <- max(range_without_peaks)
     threshold_ind <- nmr_dataset$axis >= r_start & nmr_dataset$axis < r_end
+    if (method == "mean3sd") {
+        cent <- mean(apply(nmr_dataset$data_1r[, threshold_ind], 2, mean))
+        disp <- mean(apply(nmr_dataset$data_1r[, threshold_ind], 2, stats::sd))
+        return(cent + 3*disp)
+    } else if (method == "median3mad") {
     out <- rep(NA_real_, nmr_dataset$num_samples)
     for (i in seq_len(nmr_dataset$num_samples)) {
         if ("data_1r_baseline" %in% names(unclass(nmr_dataset))) {
@@ -33,7 +53,10 @@ nmr_baseline_threshold <- function(nmr_dataset, range_without_peaks = c(9.5, 10)
         out[i] <- stats::median(spec_region) + 3*stats::mad(spec_region)
     }
     names(out) <- names(nmr_dataset)
-    out
+        return(out)
+    } else {
+        stop("Unexpected method", method)
+    }
 }
 
 
