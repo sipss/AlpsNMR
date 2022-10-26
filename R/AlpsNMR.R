78b8c5c54e97c95c2ac035c8048692166119d816
Sergio Oller <sergioller@gmail.com>
2022-10-26 08:41:44 +0200

    Helper function for when we want to update the demo data

--- R/AlpsNMR.R
+++ R/AlpsNMR.R
@@ -52,3 +52,26 @@ magrittr::`%>%`
 #' @importFrom utils .DollarNames
 #' @export
 utils::.DollarNames
+
+
+
+.refresh_nmr_dataset_rds <- function() {
+    # Run this private function to refresh the extdata/nmr_dataset.rds file
+    # if you change the demo data or the nmr_dataset object
+    dir_to_demo_dataset <- system.file("dataset-demo", package = "AlpsNMR")
+    dataset <- nmr_read_samples_dir(dir_to_demo_dataset)
+    dataset_1D <- nmr_interpolate_1D(dataset, axis = c(min = 1, max = 5, by = 0.02))
+    # annot_fn <- system.file("dataset-demo", "dummy_metadata.xlsx", package = "AlpsNMR")
+    # annotations <- readxl::read_excel(annot_fn)
+    annotations <- 
+        structure(
+            list(NMRExperiment = c("10", "20", "30"),
+                 SubjectID = c("Ana", "Ana", "Elia"),
+                 TimePoint = c("baseline", "3 months", "baseline")
+            ),
+            class = c("tbl_df", "tbl", "data.frame"),
+            row.names = c(NA, -3L)
+        )
+    dataset_1D <- nmr_meta_add(dataset_1D, metadata = annotations, by = "NMRExperiment")
+    nmr_dataset_save(nmr_dataset = dataset_1D, "inst/extdata/nmr_dataset.rds")
+}
