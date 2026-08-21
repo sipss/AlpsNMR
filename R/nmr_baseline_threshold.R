sipss/AlpsNMR

commit 7749c1e687fe94013ec6bfb0c7f076661530492a

Make sure baseline estimation is used when thresholding

R/nmr_baseline_threshold.R
@@ -36,7 +36,6 @@
 #' bl_threshold <- nmr_baseline_threshold(dataset_1D, range_without_peaks = c(9.5,10))
 #'
 nmr_baseline_threshold <- function(nmr_dataset, range_without_peaks = NULL, method = c("mean3sd", "median3mad")) {
-    # FIXME: Maybe a whole baseline would be better, so we can cope with slowly changing baselines better
     method <- match.arg(method)
     if (is.null(range_without_peaks)) {
         cli::cli_abort(
@@ -62,23 +61,23 @@ nmr_baseline_threshold <- function(nmr_dataset, range_without_peaks = NULL, meth
             )
         )
     }
+    mat <- nmr_dataset$data1r[, threshold_ind, drop = FALSE]
+    if ("data_1r_baseline" %in% names(unclass(nmr_dataset))) {
+       mat <- mat - nmr_dataset$data_1r_baseline[, threshold_ind, drop = FALSE]
+    }
     if (method == "mean3sd") {
         if (nmr_dataset$num_samples > 1) {
-            cent <- mean(apply(nmr_dataset$data_1r[, threshold_ind, drop = FALSE], 2, mean))
-            disp <- mean(apply(nmr_dataset$data_1r[, threshold_ind, drop = FALSE], 2, stats::sd))
+            cent <- mean(apply(mat, 2, mean))
+            disp <- mean(apply(mat, 2, stats::sd))
         } else {
-            cent <- mean(as.numeric(nmr_dataset$data_1r[, threshold_ind]))
-            disp <- stats::sd(as.numeric(nmr_dataset$data_1r[, threshold_ind]))
+            cent <- mean(as.numeric(mat))
+            disp <- stats::sd(as.numeric(mat))
         }
         return(cent + 3 * disp)
     } else if (method == "median3mad") {
         out <- rep(NA_real_, nmr_dataset$num_samples)
         for (i in seq_len(nmr_dataset$num_samples)) {
-            if ("data_1r_baseline" %in% names(unclass(nmr_dataset))) {
-                spec_region <- nmr_dataset$data_1r[i, threshold_ind, drop = FALSE] - nmr_dataset$data_1r_baseline[i, threshold_ind]
-            } else {
-                spec_region <- nmr_dataset$data_1r[i, threshold_ind, drop = FALSE]
-            }
+            spec_region <- mat[i, threshold_ind, drop = FALSE]
             out[i] <- stats::median(spec_region) + 3 * stats::mad(spec_region)
         }
         names(out) <- names(nmr_dataset)

commit ee80ff9077200d9760707b25a4485256d0273829

Fix two bugs in nmr_baseline_threshold() introduced in 7749c1e
$data1r doesn't exist (the field is $data_1r), so mat silently became NULL, then NULL-minus-baseline lost its matrix dims via recycling. mat is already restricted to threshold_ind columns before the median3mad loop, so re-indexing by threshold_ind there was a second, redundant subscript that errored once mat regained the right dims. Both methods now verified end-to-end.

R/nmr_baseline_threshold.R
@@ -61,7 +61,7 @@ nmr_baseline_threshold <- function(nmr_dataset, range_without_peaks = NULL, meth
             )
         )
     }
-    mat <- nmr_dataset$data1r[, threshold_ind, drop = FALSE]
+    mat <- nmr_dataset$data_1r[, threshold_ind, drop = FALSE]
     if ("data_1r_baseline" %in% names(unclass(nmr_dataset))) {
        mat <- mat - nmr_dataset$data_1r_baseline[, threshold_ind, drop = FALSE]
     }
@@ -77,7 +77,7 @@ nmr_baseline_threshold <- function(nmr_dataset, range_without_peaks = NULL, meth
     } else if (method == "median3mad") {
         out <- rep(NA_real_, nmr_dataset$num_samples)
         for (i in seq_len(nmr_dataset$num_samples)) {
-            spec_region <- mat[i, threshold_ind, drop = FALSE]
+            spec_region <- mat[i, , drop = FALSE]
             out[i] <- stats::median(spec_region) + 3 * stats::mad(spec_region)
         }
         names(out) <- names(nmr_dataset)